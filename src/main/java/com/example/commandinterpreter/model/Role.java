58f0c3f2701eaf157d1476cd089bc7aed4068f88
Marwa Sajjad <marwakhaan10@gmail.com>
2026-01-08 00:57:50 +0500

    Updated the register Interface

--- src/main/java/com/example/commandinterpreter/model/Role.java
+++ src/main/java/com/example/commandinterpreter/model/Role.java
@@ -2,6 +2,6 @@ package com.example.commandinterpreter.model;
 
 // Enum for user roles (Polymorphism: Different behavior based on role)
 public enum Role {
-    USER,  // Basic access
-    ADMIN  // Full access (e.g., power commands)
+    USER,
+    ADMIN
 }
